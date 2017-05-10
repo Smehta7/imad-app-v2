3b37ccde05c3d38653a6861e4333224cfa8ba56e
Smehta7 <sanketmehta765@gmail.com>
2017-05-10 15:44:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 
 var el = document.getElementById('mt');
 el.innerHTML = 'MODI_FIED';
+
+//move image on click
+var im = document.getElementById('m') ;
+im.onclick = function() {
+    img.style.marginLeft = '100px' ;
+};
